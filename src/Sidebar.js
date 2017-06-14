c945138d4ca3048f05bfad6a801e1bc7e10fb552
Brandon Loi <brandon.loi@gmail.com>
2017-06-14 14:22:13 -0400

    Add css to components

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,16 +6,13 @@ import quill from './quill.svg'
 const SideBar = () => {
   return (
     <div className="Sidebar">
-    <nav class="Nav">
-      <div class="logo">
+      <div className="logo">
         <img src={quill} alt="Noteherder"/>
       </div>
-      <button class="new-note">
+      <button className="new-note">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new-hover.png" alt="New note"/>
-        <img class="outline" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new.png" alt="New note"/>
+        <img className="outline" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new.png" alt="New note"/>
       </button>
-    </nav>
-
     </div>
   )
 }
