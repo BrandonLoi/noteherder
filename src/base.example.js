e67b43042b6d71614e3ca22a770b29a6c23ec2dd
Brandon Loi <brandon.loi@gmail.com>
2017-06-20 10:18:33 -0400

    Add google auth

--- src/base.example.js
+++ src/base.example.js
@@ -1,17 +1,21 @@
 import Rebase from 're-base'
 import firebase from 'firebase/app'
 import database from 'firebase/database'
+import 'firebase/auth'
 
-const app = firebase.initializeApp(
-  {
-    apiKey: "YOUR API KEY HERE",
-    authDomain: "YOUR-APP.firebaseapp.com",
-    databaseURL: "YOUR-APP.firebaseio.com",
-    projectId: "YOUR-APP",
-    storageBucket: "YOUR-APP.appspot.com",
-    messagingSenderId: "1019802345500"
-  })
+const app = firebase.initializeApp({
+  apiKey: "YOUR API KEY HERE",
+  authDomain: "YOUR-APP.firebaseapp.com",
+  databaseURL: "https://YOUR-APP.firebaseio.com",
+  projectId: "YOUR-APP",
+  storageBucket: "YOUR-STORAGE-BUCKET",
+  messagingSenderId: "YOUR MESSAGING SENDER ID"
+})
 
-  const db = database(app)
+const db = database(app)
 
-  export default Rebase.createClass(db)
+export const auth = app.auth()
+export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export default Rebase.createClass(db)
